e0d949354c6cb11e8f00b08ac162724cccb22bbc
eric-martinez <ericdemian.martinez@gmail.com>
2020-12-02 08:47:17 -0600

    Practice done

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BandNamesApp } from './BandNamesApp';
+import './index.css';
+
+
+ReactDOM.render(
+  <BandNamesApp />,
+  document.getElementById('root')
+);
